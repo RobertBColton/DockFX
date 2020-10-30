9278241608326df77c85ccd502ce1de443cb48f5
Robert Colton <robertbcolton@gmail.com>
2020-10-29 22:07:33 -0400

    Update DockPos.java

--- src/main/java/org/dockfx/DockPos.java
+++ src/main/java/org/dockfx/DockPos.java
@@ -3,19 +3,9 @@
  * @brief Enumeration of dock area alignment constants.
  *
  * @section License
- *
- *          This file is a part of the DockFX Library. Copyright (C) 2015 Robert B. Colton
- *
- *          This program is free software: you can redistribute it and/or modify it under the terms
- *          of the GNU Lesser General Public License as published by the Free Software Foundation,
- *          either version 3 of the License, or (at your option) any later version.
- *
- *          This program is distributed in the hope that it will be useful, but WITHOUT ANY
- *          WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- *          PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
- *
- *          You should have received a copy of the GNU Lesser General Public License along with this
- *          program. If not, see <http://www.gnu.org/licenses/>.
+ *          This Source Code Form is subject to the terms of the Mozilla Public
+ *          License, v. 2.0. If a copy of the MPL was not distributed with this
+ *          file, You can obtain one at https://mozilla.org/MPL/2.0/.
  **/
 
 package org.dockfx;
